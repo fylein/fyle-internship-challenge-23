50e518ecd34582b0a99b0504c87f48eac6467f65
mythilisivasamy <k_mytili@yahoo.com>
2023-11-07 10:35:17 +0530

    first-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,55 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from './services/api.service';
+import { USER_INFO,REPO } from './services/data';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit{
-  constructor(
-    private apiService: ApiService
-  ) {}
-
+export class AppComponent implements OnInit {
+  title:string='fyle-frontend-challenge';
+  userName: string = '';
+  user$:USER_INFO={
+    login:'',
+    bio:'',
+    location:'',
+    twitter_user:''
+  };
+  repo$!:REPO[];
+  isLoaded:boolean=true;
+  showSkeleton!:boolean;
+  constructor(private apiService: ApiService) {}
   ngOnInit() {
-    this.apiService.getUser('johnpapa').subscribe(console.log);
+    this.showSkeleton=true
+}
+  
+  handleChange(name: string) {
+    this.isLoaded=false;
+    this.userName = name;
+    console.log(this.userName);
+    this.apiService
+      .getUser(this.userName)
+      .subscribe((user:any) => {
+        console.log(user)
+      this.isLoaded=true;
+      this.showSkeleton=false;
+          this.user$={
+            login:user.login,
+            bio:user.bio,
+            location:user.location,
+            twitter_user:user.twitter_user
+          }
+        });
+        this.apiService.getRepo(this.userName)
+        .subscribe(
+          (repos:any)=>{
+            repos.map((repo:any)=>{
+              this.repo$.push({name:repo.name,language:repo.language,description:repo.description})
+            })
+          }
+        )
   }
+  
+  
 }
---
+++ src/app/services/data.ts
@@ -0,0 +1,12 @@
+export interface USER_INFO {
+  login: string;
+  bio: string;
+  location: string;
+  twitter_user: string;
+}
+
+export interface REPO {
+  name?: string;
+  description?: string;
+  language?: string;
+}
