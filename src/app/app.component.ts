1dcaa7803554cbd973f69a99348b44cb75110147
Harshit Singh <harshitsingh467@gmail.com>
2023-11-23 16:37:35 +0530

    Added import modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,68 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from './services/api.service';
-import { HttpClientModule } from '@angular/common/http';
-import { NgxUiLoaderHttpModule, NgxUiLoaderModule } from 'ngx-ui-loader';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-
+import { User } from './user';
+import { Repo } from './repo';
+import { PageEvent } from '@angular/material/paginator';
+import { NONE_TYPE } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
-username: any;
-onSubmit() {
-throw new Error('Method not implemented.');
-}
-title: any;
-renderDetails: any;
-input: any;
-gitHubUsername: any;
-repos: any;
-  constructor(
-    private apiService: ApiService
-  ) {}
+export class AppComponent implements OnInit {
 
-  ngOnInit() {
-    this.apiService.getUser('johnpapa').subscribe(console.log);
+  title = 'fyle-frontend-challenge';
+  input = '';
+  gitHubUsername = '';
+
+  user!: User;
+  repos: Repo[] = [];
+  perPage = 10;
+  pageIndex = 1;
+
+  ngOnInit() { }
+
+  renderDetails() {
+    if (!this.input) {
+      this.clearInputs();
+      return;
+    }
+    this.getUserDetails(this.input);
+    this.gitHubUsername = this.input;
+  }
+
+  handlePageEvent(event: PageEvent) {
+    this.perPage = event.pageSize;
+    this.pageIndex = event.pageIndex + 1;
+    this.getUserRepos(this.user.login);
+  }
+
+  constructor(private api: ApiService) { }
+
+  private clearInputs() {
+    this.input = '';
+    this.gitHubUsername = '';
+    this.user = {} as User;
+    this.repos = [];
+  }
+
+  private getUserDetails(username: string): void {
+    this.api.getUser(username).subscribe({
+      next: (response) => {
+        this.user = response;
+        this.getUserRepos(this.user.login);
+      },
+      error: (err) => { console.error('Error fetching user:', err); },
+      complete: () => { console.log('Fetched user details successfully'); }
+    });
+  }
+
+  private getUserRepos(username: string): void {
+    this.api.getRepos(username, this.perPage, this.pageIndex).subscribe({
+      next: (response) => { this.repos = response; },
+      error: (err) => { console.error('Error fetching repos:', err); },
+      complete: () => { console.log('Fetched repos successfully', this.repos); }
+    });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,78 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from  '@angular/common/http';
+import { UserdetailComponent } from './userdetail/userdetail.component';
+import { PaginatorComponent } from './paginator/paginator.component';
+import { ApiService } from './services/api.service';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReposgridComponent } from './reposgrid/reposgrid.component';
+import { TopicsComponent } from './topics/topics.component';
+import { FormComponent } from './form/form.component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { NgxUiLoaderModule,NgxUiLoaderHttpModule  } from "ngx-ui-loader";
+import { NgxUiLoaderConfig, SPINNER, POSITION, PB_DIRECTION, } from "ngx-ui-loader";
+
+const ngxUiLoaderConfig : NgxUiLoaderConfig = {
+  blur: 5,
+  delay: 0,
+  fastFadeOut: true,
+  fgsColor: 'white',
+  fgsPosition: POSITION.centerCenter,
+  fgsSize: 20,
+  fgsType: SPINNER.rotatingPlane, // foreground spinner type
+  gap: 24,
+  logoPosition: POSITION.centerCenter,
+  logoSize: 50,
+  masterLoaderId: 'master',
+  overlayBorderRadius: '0',
+  overlayColor: 'rgba(40, 40, 40, 0.8)',
+  pbColor: 'red',
+  pbDirection: PB_DIRECTION.leftToRight, // progress bar direction
+  pbThickness: 4, // progress bar thickness
+  hasProgressBar: true,
+  text: 'Loading',
+  textColor: '#FFFFFF',
+  textPosition: POSITION.centerCenter,
+  maxTime: -1,
+  minTime: 200,
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserdetailComponent,
+    PaginatorComponent,
+    ReposgridComponent,
+    TopicsComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    MatChipsModule,
+    MatPaginatorModule,
+    MatGridListModule,
+    BrowserAnimationsModule,
+    MatChipsModule,
+    MatFormFieldModule, 
+    MatInputModule, 
+    FormsModule, 
+    MatButtonModule, 
+    MatIconModule,
+    NgxUiLoaderModule.forRoot(ngxUiLoaderConfig),
+    NgxUiLoaderHttpModule.forRoot({showForeground: true})
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ApiService],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule { }
