75cd6edd9aacadaeea181f246d305953428a9a52
System Administrator <coolest.nikhil2000@gmail.com>
2023-11-27 21:42:01 +0530

    Added some test cases for api service and one component

--- src/app/profile/profile.component.spec.ts
+++ src/app/profile/profile.component.spec.ts
@@ -1,21 +1,58 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { of } from 'rxjs';
 import { ProfileComponent } from './profile.component';
+import { ApiService } from '../services/api.service';
 
 describe('ProfileComponent', () => {
   let component: ProfileComponent;
   let fixture: ComponentFixture<ProfileComponent>;
+  let apiService: jasmine.SpyObj<ApiService>;
 
   beforeEach(() => {
+    const spy = jasmine.createSpyObj('ApiService', ['getSearchTerm', 'getUser', 'setTotalPages', 'setCurrPage']);
+
     TestBed.configureTestingModule({
-      declarations: [ProfileComponent]
+      declarations: [ProfileComponent],
+      providers: [{ provide: ApiService, useValue: spy }],
     });
+
     fixture = TestBed.createComponent(ProfileComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    apiService = TestBed.inject(ApiService) as jasmine.SpyObj<ApiService>;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should initialize and subscribe to getSearchTerm', () => {
+    const searchTerm = 'testUser';
+    apiService.getSearchTerm.and.returnValue(of(searchTerm));
+
+    fixture.detectChanges();
+
+    expect(component.isLoading).toBe(true);
+    expect(apiService.getSearchTerm).toHaveBeenCalled();
+    expect(apiService.getUser).toHaveBeenCalledWith(); // getUser should be called on subscription to getSearchTerm
+    expect(component.isLoading).toBe(false);
+  });
+
+  it('should call getUser and update user information', () => {
+    const mockUser = { login: 'testUser', public_repos: 10 };
+
+    apiService.getSearchTerm.and.returnValue(of('testUser'));
+    apiService.getUser.and.returnValue(of(mockUser));
+
+    fixture.detectChanges();
+
+    component.getUser(); // Manually call getUser
+
+    expect(component.isLoading).toBe(true);
+    expect(apiService.getUser).toHaveBeenCalled();
+    expect(component.user).toEqual(mockUser);
+    expect(apiService.setTotalPages).toHaveBeenCalledWith(2); // Math.floor((10 + 7) / 8) = 2
+    expect(apiService.setCurrPage).toHaveBeenCalledWith(1);
+    expect(component.isLoading).toBe(false);
+  });
+
 });
--- src/app/repos/repos.component.spec.ts
+++ src/app/repos/repos.component.spec.ts
@@ -1,21 +1,50 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { of } from 'rxjs';
 
 import { ReposComponent } from './repos.component';
+import { ApiService } from '../services/api.service';
 
 describe('ReposComponent', () => {
   let component: ReposComponent;
   let fixture: ComponentFixture<ReposComponent>;
+  let apiService: jasmine.SpyObj<ApiService>;
 
   beforeEach(() => {
+    //a spy object for ApiService with a 'getLanguages' method
+    const spy = jasmine.createSpyObj('ApiService', ['getLanguages']);
+
+    // Configure the testing module
     TestBed.configureTestingModule({
-      declarations: [ReposComponent]
+      declarations: [ReposComponent],
+      imports: [HttpClientTestingModule], 
+      providers: [{ provide: ApiService, useValue: spy }],
     });
+
     fixture = TestBed.createComponent(ReposComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+
+    apiService = TestBed.inject(ApiService) as jasmine.SpyObj<ApiService>;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should fetch and populate languages array', () => {
+    // Mock response from the getLanguages method
+    const mockLanguages = { lang1: 100, lang2: 50, lang3: 30 };
+
+    component.languages_url = 'fake-url';
+
+    component.ngOnInit();
+
+    fixture.detectChanges();
+
+    expect(apiService.getLanguages).toHaveBeenCalledWith('fake-url');
+    
+    // Modify this expectation to match the expected structure of your component.languages array
+    expect(component.languages.length).toBe(3);
+    expect(component.languages).toEqual(['lang1', 'lang2', 'lang3']);
   });
 });
