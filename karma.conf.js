88c903ad4f5974273d226cfaf883cafd1ed3b3cd
L K Livingstone <livingstone.lk.337@gmail.com>
2023-12-17 00:02:47 +0530

    Changed karma and package json

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       customLaunchers: {
         ChromeHeadlessNoSandbox: {
           base: 'ChromeHeadless',
-          flags: ['--no-sandbox'],
+          flags: ['--no-sandbox', '--disable-gpu'],
         },
       },
       singleRun: false,
