f34732f9f63c8ad38ed09c9f73070fa1047a7147
L K Livingstone <livingstone.lk.337@gmail.com>
2023-12-16 23:53:19 +0530

    Added karma.conf.js

---
+++ karma.conf.js
@@ -0,0 +1,35 @@
+module.exports = function (config) {
+    config.set({
+      basePath: '',
+      frameworks: ['jasmine', '@angular-devkit/build-angular'],
+      plugins: [
+        require('karma-jasmine'),
+        require('karma-chrome-launcher'),
+        require('karma-coverage-istanbul-reporter'),
+        require('@angular-devkit/build-angular/plugins/karma'),
+      ],
+      client: {
+        clearContext: false,
+      },
+      coverageIstanbulReporter: {
+        dir: require('path').join(__dirname, './coverage/your-app-name'),
+        reports: ['html', 'lcovonly', 'text-summary'],
+        fixWebpackSourcePaths: true,
+      },
+      reporters: ['progress', 'kjhtml'],
+      port: 9876,
+      colors: true,
+      logLevel: config.LOG_INFO,
+      autoWatch: true,
+      browsers: ['ChromiumHeadless'],
+      customLaunchers: {
+        ChromiumHeadless: {
+          base: 'Chromium',
+          flags: ['--headless', '--disable-gpu', '--no-sandbox', '--disable-software-rasterizer'],
+        },
+      },
+      singleRun: false,
+      restartOnFileChange: true,
+    });
+  };
+  
