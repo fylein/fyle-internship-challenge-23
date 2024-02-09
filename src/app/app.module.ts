8c21d8497d2cae8a7a6fc0927e64cd4ce596962e
Mohit Yadav <96173191+mohit8890@users.noreply.github.com>
2024-02-09 20:55:37 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { FormsModule } from '@angular/forms';
+
+import { ProfileService } from './services/api.service';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from  '@angular/common/http';
+import { ProfileComponent } from './components/profile/profile.component';
+
+import { MatTableModule } from '@angular/material/table';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
+    AppComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    MatTableModule,
+    BrowserAnimationsModule
   ],
   providers: [],
+  providers: [ProfileService],
   bootstrap: [AppComponent]
 })
+export class AppModule { }
 export class AppModule { }
