467f73586f2f6e86dffa0150bb2ed6bded73f06f
arindam <arindam0805@gmail.com>
2024-02-11 16:05:51 +0530

    modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,26 @@ import { ApiService } from './services/api.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
+  username: string = '';
+  userData: any; 
   constructor(
     private apiService: ApiService
   ) {}
 
   ngOnInit() {
     this.apiService.getUser('johnpapa').subscribe(console.log);
+  }
+  onSubmit() {
+    this.getUserData(this.username);
+  }
+  getUserData(username: string) {
+    this.apiService.getUser(username).subscribe(
+      (data: any) => {
+        this.userData = data;
+      },
+      (error) => {
+        console.error('Error fetching user data:', error);
+      }
+    )
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from  '@angular/common/http';
 
@@ -10,7 +10,8 @@ import { HttpClientModule } from  '@angular/common/http';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
