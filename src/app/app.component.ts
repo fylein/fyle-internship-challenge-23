beb44a824165937290ec5d70dd8cb87733bf60b4
teja <320106410037@andhrauniversity.edu.in>
2024-02-12 18:44:34 +0530

    gitDisplay Angular App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,14 +4,62 @@ import { ApiService } from './services/api.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit{
-  constructor(
-    private apiService: ApiService
-  ) {}
+export class AppComponent implements OnInit {
+  constructor(private apiService: ApiService) {}
+
+  repos: any;
+  username: string = '';
+  userData: any = {
+    public_repos: 0,
+  };
+
+  numberOfReposOptions = [5, 10, 20, 50, 100];
+  totalRepos!: number;
+  reposPerPage = 5;
+  currentPage = 1;
 
   ngOnInit() {
-    this.apiService.getUser('johnpapa').subscribe(console.log);
+    this.totalRepos = this.userData.public_repos || 0;
+
+    this.reposPerPage = 5;
+    this.currentPage = 1;
+  }
+  ngOnChanges() {
+    this.fetchBio();
+    this.fetchRepos();
+  }
+
+  fetchBio(): void {
+    this.apiService.getBio(this.username).subscribe((data) => {
+      this.userData = data;
+      this.totalRepos = this.userData.public_repos;
+    });
+  }
+
+  fetchRepos() {
+    this.apiService
+      .getUser(this.username, this.currentPage, this.reposPerPage)
+      .subscribe((res: any) => {
+        this.repos = res;
+      });
+  }
+
+  onSubmit() {
+    if (this.username.length > 0) {
+      this.fetchBio();
+      this.fetchRepos();
+    }
+  }
+  onChangeNumberOfRepos(option: number) {
+    this.reposPerPage = option;
+    console.log(this.reposPerPage, option);
+  }
+
+  onPageChanged(event: any) {
+    this.currentPage = event;
+
+    this.fetchRepos();
   }
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,19 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { tap, throwError } from 'rxjs';
+import { Observable, tap, throwError } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor(
-    private httpClient: HttpClient
-  ) { }
-
-  getUser(githubUsername: string) {
-    return this.httpClient.get(`https://api.github.com/users/${githubUsername}`);
+  getUser(githubUsername: string, currentPage: number, reposPerPage: number) {
+    return this.httpClient.get(
+      `https://api.github.com/users/${githubUsername}/repos?page=${currentPage}&per_page=${reposPerPage}`
+    );
   }
 
-  // implement getRepos method by referring to the documentation. Add proper types for the return type and params 
+  getBio(username: string): Observable<any> {
+    return this.httpClient.get(`https://api.github.com/users/${username}`);
+  }
+  // implement getRepos method by referring to the documentation. Add proper types for the return type and params
 }
