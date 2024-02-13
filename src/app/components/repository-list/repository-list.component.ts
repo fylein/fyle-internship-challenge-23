c424dd5a6612a7a31d8ba44dde6a400cfa826644
Nandeesh P S <nandish0602@gmail.com>
2024-02-13 20:26:45 +0530

    upload for refrence

---
+++ src/app/components/repository-list/repository-list.component.ts
@@ -0,0 +1,57 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-repository-list',
+  templateUrl: './repository-list.component.html',
+  styleUrls: ['./repository-list.component.scss']
+})
+export class RepositoryListComponent  {
+  @Input() repositories: any[] = []; // Your repository data
+  @Input() loading: boolean = false; // Loading flag
+  @Input() error: boolean = false;   // Error flag
+
+  currentPage: number = 1;
+  itemsPerPage: number = 6; // Display six repositories per page
+
+  get totalItems(): number {
+    return this.repositories.length;
+  }
+
+  get totalPages(): number {
+    return Math.ceil(this.totalItems / this.itemsPerPage);
+  }
+
+  get pages(): number[] {
+    return Array.from({ length: this.totalPages }, (_, index) => index + 1);
+  }
+
+
+  get displayedRepositories(): any[] {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = Math.min(startIndex + this.itemsPerPage, this.totalItems);
+    return this.repositories.slice(startIndex, endIndex);
+  }
+
+  previousPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+    }
+  }
+
+  nextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+    }
+  }
+
+  goToPage(page: number) {
+    if (page >= 1 && page <= this.totalPages) {
+      this.currentPage = page;
+    }
+  }
+
+  
+  // Add your data fetching logic or API calls here
+  // ...
+
+}
---
+++ src/app/components/user-details/user-details.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-user-details',
+  templateUrl: './user-details.component.html',
+  styleUrls: ['./user-details.component.scss']
+})
+export class UserDetailsComponent {
+  @Input() user: any;
+}
