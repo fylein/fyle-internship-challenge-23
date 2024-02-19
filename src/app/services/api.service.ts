a60c32dc49dc268b500faf20a5b86fd46d95a3cd
karthik-924 <20p61a1240@vbithyd.ac.in>
2024-02-19 21:54:20 +0530

    Removal

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -20,7 +20,6 @@ export class ApiService {
     return this.httpClient.get(`https://api.github.com/users/${githubUsername}`);
   }
 
-  // implement getRepos method by referring to the documentation. Add proper types for the return type and params 
   getRepos(githubUsername: string, page: number, pageSize: number) {
     return this.httpClient.get(`https://api.github.com/users/${githubUsername}/repos?page=${page}&per_page=${pageSize}&sort=created&direction=asc`);
   }
